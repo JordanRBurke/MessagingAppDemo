8a7ac3e8ba85c4490b3bbc265a3e9d36e7337bec
JordanRBurke <jordanrobertburke@gmail.com>
2018-03-25 11:07:04 -0400

    Added activity that allows user to input rank from game and have a picture of the rank show up on the bottom of the message.

--- app/src/main/java/com/jordanburke/messagingappdemo/InsertRankActivity.java
+++ app/src/main/java/com/jordanburke/messagingappdemo/InsertRankActivity.java
@@ -1,5 +1,6 @@
 package com.jordanburke.messagingappdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,6 +16,8 @@ public class InsertRankActivity extends AppCompatActivity {
     protected EditText rankInput;
     @BindView(R.id.add_rank_button)
     protected Button addRank;
+    @BindView(R.id.rank_button_image)
+    protected Button siegeRank;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,15 @@ public class InsertRankActivity extends AppCompatActivity {
     }
     @OnClick(R.id.add_rank_button)
     protected void rankButtonPress() {
-
         String rankStringInput = rankInput.getText().toString();
+        if (rankStringInput.equals("SILVER4")) {
+            siegeRank.setBackground(getResources().getDrawable(R.drawable.silver_four_rank));
+        } 
+
+
+        Intent toMessage = new Intent(this, MessagingActivity.class);
+//        toMessage.putExtra("RANK_VALUE", rankStringInput);
+        startActivity(toMessage);
     }
 
 }
